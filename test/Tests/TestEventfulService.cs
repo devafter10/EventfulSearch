48a85e89d22b6c685904833099700f76de69fdda
DevAfter10 <devafter10@gmail.com>
2015-02-13 04:19:27 -0800

    fix and add more unit tests

--- test/Tests/TestEventfulService.cs
+++ test/Tests/TestEventfulService.cs
@@ -4,25 +4,89 @@ using EventfulSearch.Services;
 using EventfulSearch.Models;
 using System.Linq;
 using System.Diagnostics;
+using System.Threading.Tasks;
+using Moq;
+using RestSharp;
 
 namespace Tests
 {
     public class TestEventfulService
     {
+		private class TestData
+		{
+			public int Total { get; set; }
+			public int Times { get; set; }
+			public string Message { get; set; }
+		}
+
 		[Fact()]
-	    public void TestService()
+	    public void TestGetEvents()
 		{
 			var svc = new EventfulService(new RestSharpProxy());
 			var search = new SearchRequest()
 			{
-				Address = "Vancouver",
+				Geocode = "45.6387281,-122.6614861",
+				Address = "45.6387281,-122.6614861",
 				Category = "Music",
-				StartDate = new DateTime(2015, 1, 1),
-				EndDate = new DateTime(2015, 1, 1),
-				Radius = 1f
+				StartDate = new DateTime(2015, 2, 1),
+				EndDate = new DateTime(2015, 2, 1),
+				Radius = 1f,
 			};
 			
-			var events = svc.GetEventsAsync(search);
+			var events = svc.GetEvents(search);
+			Assert.Equal(5, events.Count);
+		}
+
+		[Fact()]
+		public void TestGetTotal()
+		{
+			var svc = new EventfulService(new RestSharpProxy());
+			var search = new SearchRequest()
+			{
+				Geocode = "45.6387281,-122.6614861",
+				Address = "45.6387281,-122.6614861",
+                Category = "Music",
+				StartDate = new DateTime(2015, 2, 1),
+				EndDate = new DateTime(2015, 2, 1),
+				Radius = 1f
+			};
+
+			var count = svc.GetEventCount(search);
+			Assert.Equal(5, count);
+		}
+
+		[Fact()]
+		public void TestNumberOfCallsMade()
+		{
+			var data = new TestData[] {
+				new TestData{ Total = 0, Times = 0, Message = "zero" },
+				new TestData{ Total = 2, Times = 1, Message = "once" },
+				new TestData{ Total = 99, Times = 1, Message = "once" },
+				new TestData{ Total = 100, Times = 1, Message = "exactly once" },
+				new TestData{ Total = 101, Times = 2, Message = "twice" }
+			};
+
+			foreach (var d in data)
+			{
+				TestNumberOfCallsMadeCore(d);
+			}
+		}
+
+		private void TestNumberOfCallsMadeCore(TestData d)
+		{
+			Console.WriteLine(d.Message);
+
+			var req = new EventfulResponse() { TotalItems = d.Total };
+
+			var mock = new Mock<IRestProxy>();
+			// setup total count number
+			mock.Setup(p => p.Execute<EventfulResponse>(It.IsAny<RestRequest>()))
+				.Returns(req);
+
+			var svc = new EventfulService(mock.Object);
+			var tasks = svc.GetEventsAsync(new SearchRequest());
+
+			mock.Verify(p => p.ExecuteAsync<EventfulResponse>(It.IsAny<RestRequest>()), Times.Exactly(d.Times));
 		}
 
 		[Fact()]
@@ -34,6 +98,5 @@ namespace Tests
 
 			Assert.Equal("2015-01-24 13:49:00", str);
         }
-
     }
 }
---
+++ test/Tests/TestSearchHelper.cs
@@ -0,0 +1,122 @@
+﻿using EventfulSearch.Models;
+using EventfulSearch.Services;
+using Moq;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class TestSearchHelper
+    {
+		SearchHelper _helper;
+		Mock<IGoogleGeocodeService> _mock = new Mock<IGoogleGeocodeService>();
+
+		GeocodeResponse goodStatus = new GeocodeResponse()
+		{
+			Latitude = "1234",
+			Longitude = "-1234",
+			Status = "OK"
+		};
+
+		GeocodeResponse badStatus = new GeocodeResponse()
+		{
+			Latitude = "1234",
+			Longitude = "-1234",
+			Status = "Bad"
+		};
+
+		GeocodeResponse badLat = new GeocodeResponse()
+		{
+			Latitude = "",
+			Longitude = "-1234",
+			Status = "OK"
+		};
+
+		GeocodeResponse badLon = new GeocodeResponse()
+		{
+			Latitude = "1234",
+			Longitude = "",
+			Status = "OK"
+		};
+
+		public TestSearchHelper()
+		{
+		}
+
+		[Fact]
+		public void TestBadStatus()
+		{
+			_mock.Setup(s => s.GetGeocode("badStatus")).Returns(badStatus);
+			_helper = new SearchHelper(_mock.Object);
+
+			var ret = _helper.IsAddressValid("badStatus").Item1;
+			Assert.False(ret);
+		}
+
+		[Fact]
+		public void TestBadLat()
+		{
+			_mock.Setup(s => s.GetGeocode("badLat")).Returns(badLat);
+			_helper = new SearchHelper(_mock.Object);
+
+			var ret = _helper.IsAddressValid("badLat").Item1;
+			Assert.False(ret);
+		}
+
+		[Fact]
+		public void TestBadLon()
+		{
+			_mock.Setup(s => s.GetGeocode("badLon")).Returns(badLon);
+			_helper = new SearchHelper(_mock.Object);
+
+			var ret = _helper.IsAddressValid("badLon").Item1;
+			Assert.False(ret);
+		}
+
+		[Fact]
+		public void TestGoodGeocode()
+		{
+			_mock.Setup(s => s.GetGeocode("goodStatus")).Returns(goodStatus);
+			_helper = new SearchHelper(_mock.Object);
+
+			var ret = _helper.IsAddressValid("goodStatus");
+			Assert.True(ret.Item1);
+			Assert.Equal("1234,-1234", ret.Item2);
+		}
+
+		[Fact]
+		public void TestIsValid()
+		{
+			Console.WriteLine("empty category (aka optional)");
+			TestIsValidCore("", true);
+
+			Console.WriteLine("mixed case");
+			TestIsValidCore("mUSic", true);
+
+			Console.WriteLine("mystery category");
+			TestIsValidCore("does not exist", false);
+		}
+
+		public void TestIsValidCore(string category, bool expectTrue)
+		{
+			_mock.Setup(s => s.GetGeocode("goodStatus")).Returns(goodStatus);
+			_helper = new SearchHelper(_mock.Object);
+
+			var req = new SearchRequest()
+			{
+				Address = "goodStatus",
+				Category = category
+			};
+
+			var ret = _helper.IsValid(req);
+
+			if (expectTrue)
+			{
+				Assert.True(ret);
+			} else
+			{
+				Assert.False(ret);
+			}
+		}
+	}
+}
